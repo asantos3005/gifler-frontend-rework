5425c84f13e4798a7cb224f84caeb8e21f400b68
Andrew Santos <andrewsan321@gmail.com>
2025-08-20 22:52:59 +1000

    Refactored fronend - added gradient background and added modal backgrounds for form and added layouts components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './styles/App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { DefaultLayout, CenteredLayout } from './components/Layouts';
+
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Create from './pages/Create';
@@ -12,9 +14,9 @@ function App() {
       <Navbar /> {/* Always shows */}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/create" element={< Create/>} />                 
-        <Route path="/login" element={< Login/>} /> 
-        <Route path="/register" element={< Register/>} /> 
+        <Route path="/create" element={<CenteredLayout>< Create/></CenteredLayout>} />                 
+        <Route path="/login" element={<CenteredLayout>< Login/></CenteredLayout>} /> 
+        <Route path="/register" element={<CenteredLayout>< Register/></CenteredLayout>} /> 
       </Routes>
     </Router>
   );
